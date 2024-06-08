52b85660ffa7abe6de8e619c751c190bfea25051
parmargaurang <parmargaurang1111@gmail.com>
2024-06-08 22:26:50 +0530

    Add files via upload

---
+++ EmployeeDetails/src/DemoMain.java
@@ -0,0 +1,12 @@
+
+public class DemoMain {
+
+	public static void main(String[] args) {
+		Employee ep=new Employee("abc","krishna","abc",21,12456,1200);
+		System.out.println(ep);
+		Manager mp=new Manager("xyz","ram","idk",22,156345,1500);
+		System.out.println(mp);
+
+	}
+
+}
